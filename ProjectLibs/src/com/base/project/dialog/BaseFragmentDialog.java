dd6e08c909a1bdfb95a4a48e49f9eaa83ee6a86c
CuongSmile <phamcuong16021992@gmail.com>
2015-04-15 09:47:54 +0700

    Update BaseFragmentDialog.java

--- ProjectLibs/src/com/base/project/dialog/BaseFragmentDialog.java
+++ ProjectLibs/src/com/base/project/dialog/BaseFragmentDialog.java
@@ -35,12 +35,20 @@ public abstract class BaseFragmentDialog extends DialogFragment {
 	 */
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		return new Dialog(getActivity()) {
-			@Override
-			public void onBackPressed() {
-				// super.onBackPressed();
-			}
-		};
+		// the content
+		final RelativeLayout root = new RelativeLayout(getActivity());
+		root.setLayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
+		// creating the fullscreen dialog
+		final Dialog dialog = new Dialog(getActivity());
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(root);
+		dialog.getWindow().setBackgroundDrawable(
+				new ColorDrawable(Color.TRANSPARENT));
+		dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		return dialog;
 	}
 
 	@Override
